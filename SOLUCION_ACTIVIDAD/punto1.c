9142672facccc0b21dab9c446bb32f269c33e7f5
Varv0id <jairocampana10001@gmail.com>
2020-03-01 21:15:23 -0500

    [Code] Punto 1 y carpeta pantallazos

---
+++ SOLUCION_ACTIVIDAD/punto1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *   @brief  Cuenta las veces que aparece un caracter determinado dentro de una cadena.
+ *
+ *   @param  array cadena de caracteres a ingresar
+ *   @param  ch es el caracter a averiguar
+ *   @return el numero de veces que aparece ch en array o -1 si no aparece.
+ */
+int contarCaracter(char *array, char ch);
+int main(){
+    char caracteres[40];
+    printf("Porfavor ingrese una cadena de texto: ");
+    fgets(caracteres,40, stdin);
+    printf("Porfavor ingrese el caracter a contar: ");
+    char a;
+    scanf("%c",&a);
+
+
+    printf("%d \n",contarCaracter(caracteres, a));
+    return 0;
+}
+int contarCaracter(char *array, char ch) {
+  int cantidad = 0;
+  while(*array != '\0'){
+      if(array[0] == ch){
+        cantidad++;
+      }
+      array++;
+  }
+  if(cantidad != 0){
+    return cantidad;
+  }else{
+    return -1;
+  }
+  
+}
